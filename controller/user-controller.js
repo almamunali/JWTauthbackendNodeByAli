f718f239d3c88899cb0a60c750605a0cc369fecd
Sk.Almamun Ali <94174675+almamunali@users.noreply.github.com>
2023-07-22 22:24:39 +0530

    sec-commit

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,21 +1,132 @@
-const User =require('../modal/user')
+const User = require('../modal/user')
+const bcrypt = require("bcrypt");
+const jwt =require("jsonwebtoken");
+const JWT_SECRET="Alibaba"
 
 
+const signup = async (req, res, next) => {
+    const { name, email, password } = req.body
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
 
-const signup =async (req,res,next)=>{
-    const user=new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
+    } catch (err) {
+        console.log(err);
+    }
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exists" })
+    }
+
+
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+    const user = new User({
+        name,//name:name
+        email,
+        password: hashedPassword
     });
 
-    try{
+    try {
         await user.save();
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 
-    return res.status(201).json({message:user})
+    return res.status(201).json({ message: user })
 }
 
-exports.signup=signup;
+const login =async(req,res,next)=>{
+    const {email,password}=req.body;
+    let existingUser;
+    try{
+        existingUser=await User.findOne({email:email})
+
+    }catch(err){
+        return new Error(err)
+    }
+    if(!existingUser){
+        return res.status(400).json({message:"User not found.Signup Please..."})
+    }
+    const isPasswordCorrect =bcrypt.compareSync(password,existingUser.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Inavalid Email / Password"})
+    }
+
+    const token =jwt.sign({id:existingUser._id},JWT_SECRET,{expiresIn:"30s"})
+    // add cookies
+    res.cookie(String(existingUser._id),token,{
+        path:"/",
+        expires:new Date(Date.now()+1000*30),
+        httpOnly:true,
+        sameSite:"lax"
+    })
+
+    return res.status(200).json({
+        message:"Successfully logged In",user:existingUser,token
+    })
+
+
+}
+
+const verifyToken =(req,res,next)=>{
+    const cookies=req.headers.cookie;
+    const token =cookies.split("=")[1];
+    console.log("tokon",token); 
+    console.log(cookies);
+//     const headers =req.headers[`authorization`];
+//     const token =headers.split(' ')[1];
+    if(!token) {
+        res.status(404).json({message:"No token found"})
+    }
+    jwt.verify(String(token),JWT_SECRET,(err,user)=>{
+        if(err){
+          return  res.status(400).json({
+                message :"Invalid Token"
+            });
+        }
+       
+        console.log(user.id)
+        req.id=user.id;
+    });
+
+   next();
+};
+
+// const getUser = async (req,res,next)=>{
+//     const userId =req.id ;
+//     let user;
+//     try{
+//         user=await User.findOne(userId,"-password");
+//     }catch(err){
+//         return new Error(err)
+//     }
+//     if(!user){
+//         return res.status(404).json({message:"user Not found"});
+
+//     }
+//     return res.status(200).json({
+//         user
+//     })
+// }
+const getUser = async (req, res, next) => {
+    const userId = req.id;
+    let user;
+    try {
+      user = await User.findById(userId, "-password");
+    } catch (err) {
+      return new Error(err);
+    }
+    if (!user) {
+      return res.status(404).json({ messsage: "User Not FOund" });
+    }
+    return res.status(200).json({ user });
+  };
+
+
+
+
+
+exports.signup = signup;
+exports.login=login;
+exports.verifyToken=verifyToken;
+exports.getUser=getUser;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,11 @@
 const express =require("express");
-const {signup} =require('../controller/user-controller')
+const {signup,login, verifyToken, getUser} =require('../controller/user-controller')
 
 const router=express.Router();
 router.post('/signup',signup);
+router.post('/login',login);
+router.get("/user",verifyToken,getUser)
+
 
 router.get("/",(req,res,next)=>{
     res.send("Hello World")
